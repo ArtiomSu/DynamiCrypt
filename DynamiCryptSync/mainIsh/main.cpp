8a1c7fa22abd17b31518275ade96a90f5c963eb6
Artiom <artiom.sumigora@mycit.ie>
2019-02-25 21:11:15 +0000

    works ok but buffers writing on top of each other need to  make system for multiple read and write buffers

--- DynamiCryptSync/mainIsh/main.cpp
+++ DynamiCryptSync/mainIsh/main.cpp
@@ -21,6 +21,7 @@ using std::endl;
 
 io_service service;
 
+const int MAX_TPMS_PER_PEER = 2;
 
 class peer;
 typedef std::vector<boost::shared_ptr<peer>> array; // array of shared pointers to talk_to_client class
@@ -70,6 +71,11 @@ public:
         return partner_tpm;
     }
     
+    void reset(){
+        iteration_ = 0;
+        
+    }
+    
 private:
     int iteration_;
     int tpm_id_;
@@ -78,22 +84,119 @@ private:
     
 };
 
-/*
-class tpm_network_handler{
 
+class tpm_network_handler{
+public:
+    int max_iterations = 5000;
+    tpm_network_handler(){
+        srand(123456789); // consistent messages
+    }
+    
+    int create_new_tpm(){
+        int randomId = rand()%5000;
+        while(find_tpm(randomId, false) != -1){
+            randomId = rand()%5000;
+        }
+        
+        tpm_networks_.push_back(single_tpm_network_handler(randomId, max_iterations));
+        std::cout << "created new tpm with id " << randomId << std::endl;
+        return randomId;
+    }
+    
+    int find_tpm(int id, bool find_partner){
+        for(int i=0; i < tpm_networks_.size(); i++){
+            
+            if(find_partner){
+                if(tpm_networks_[i].partner() == id){
+
+                    return i;
+                }
+            } else {
+                
+                if(tpm_networks_[i].id() == id){
+
+                    return i;
+                }
+                
+            }
+        }
+        return -1;
+    }
+    
+    int increase_iteration(int id){
+        int index = find_tpm(id, false);
+        if(tpm_networks_[index].iteration_increase()){
+            
+            return 1;
+        }else{
+            reset_tpm(index);
+            //tpm_networks_.erase(tpm_networks_.begin()+index);
+            return 0; 
+        }
+            
+    }
+    
+    int reset_tpm(int index){
+        tpm_networks_[index].reset();
+        return 1;
+    }
+    
+    int getid(int index){
+        return tpm_networks_[index].id();
+          
+    }
+    
+    int get_iteration(int id){
+        int index = find_tpm(id, false);
+        if(index == -1 ){
+            return index;
+        }
+        else{
+            return tpm_networks_[index].iteration();
+        }
+    }
+    
+    int set_partner(int id, int partner_id){
+        int index = find_tpm(id, false);
+        tpm_networks_[index].set_partner(partner_id);
+        return 1;
+    }
+    
+    int get_partner(int id){
+        int index = find_tpm(id, false);
+        if(index == -1) {
+            return index;
+        }
+        return tpm_networks_[index].partner();
+        
+    }
+    
+    single_tpm_network_handler* get_tpm(int id){
+        int index = find_tpm(id, false);
+        if(index != -1){
+           return &tpm_networks_[index];
+        } else{
+            return NULL;
+        }
+        
+    }
+    
+   
+    
     
     
     
 private:
+    std::vector<single_tpm_network_handler> tpm_networks_;
     
     
-}
-*/
+};
+
 
 class peer : public boost::enable_shared_from_this<peer>, boost::noncopyable {
     typedef peer self_type;
     peer() : sock_(service), started_(false), timer_(service), peers_changed_(false) {
-   
+        
     }
     
 public:
@@ -198,6 +301,11 @@ private:
             std::cout << "message type 3 received" << "with partner id of " << parsed_msg.at(1) << " and iteration " << parsed_msg.at(2) << " and self id of " <<  parsed_msg.at(3) << std::endl;
             on_linking(parsed_msg);
         }
+        // reset tree parity machines or stop
+        else if(std::stoi(parsed_msg.at(0)) == 4){
+            std::cout << "message type 4 received" << "with partner id of " << parsed_msg.at(1) << " and iteration " << parsed_msg.at(2) << " and stop tpm " <<  parsed_msg.at(3) << std::endl;
+            on_reset(parsed_msg);
+        }
         
         
         else std::cerr << "invalid msg " << msg << std::endl;
@@ -206,37 +314,35 @@ private:
     
     void on_connect(const error_code & err) {
         
-        tpms.push_back(single_tpm_network_handler(1234, 5000));
+        for(int b; b< MAX_TPMS_PER_PEER; b++){ //create 10 tpms
         
-        
-        if ( !err){      
-            std::stringstream ss;
-            ss << "2\t" << tpms.back().id() << "\t" << tpms.back().iteration() << "\n";
-            std::cout << "on_connect: " << ss.str() << std::endl;
-            do_write(ss.str());
-        }
-        else{
-            std::cout << "Error on_connect:" << err.message() << std::endl;
-            stop();
+            int id = tpm_handler.create_new_tpm();
+
+
+            if ( !err){      
+                std::stringstream ss;
+                ss << "2\t" << id << "\t" << tpm_handler.get_iteration(id) << "\n";
+                std::cout << "on_connect: " << ss.str() << std::endl;
+                do_write(ss.str());
+            }
+            else{
+                std::cout << "Error on_connect:" << err.message() << std::endl;
+                stop();
+            }
         }
     }
     
     void on_init(std::vector<std::string> & parsed_msg){
         
-        srand(123456789); // consistend messages
         
         
         
-        tpms.push_back(single_tpm_network_handler(rand()%5000, 5000));
-        int index = tpms.size()-1;
-        // NB should make sure this id is unique -- will do later
-        
-        
-        tpms[index].set_partner(std::stoi(parsed_msg.at(1)));
-        
+        int id = tpm_handler.create_new_tpm();
+        tpm_handler.set_partner(id, std::stoi(parsed_msg.at(1)));
+                
         
         std::stringstream ss;
-        ss << "3\t" << tpms[index].id() << "\t" << tpms[index].iteration() << "\t" << tpms[index].partner() << "\n";
+        ss << "3\t" << id << "\t" << tpm_handler.get_iteration(id) << "\t" << tpm_handler.get_partner(id) << "\n";
         std::cout << std::stoi(parsed_msg.at(1)) << ";" << parsed_msg.at(1) << " on_init: " << ss.str() << std::endl;
         do_write(ss.str());
   
@@ -244,19 +350,26 @@ private:
     
     void on_sync(std::vector<std::string> & parsed_msg){
         bool tpm_found = false;
-        int tpm_index = -1;
-        for(int i=0; i < tpms.size(); i++){
-            if(tpms.at(i).partner() == std::stoi(parsed_msg.at(1))){
-                
-                tpms.at(i).iteration_increase();
-                tpm_found = true;
-                tpm_index = i;
-                break;
+        bool tpm_reset = false;
+        int tpm_index = tpm_handler.find_tpm(std::stoi(parsed_msg.at(1)), true);
+        int tpm_id = -1;
+        if(tpm_index != -1){
+            tpm_found = true;
+            tpm_id = tpm_handler.getid(tpm_index);
+            
+            if(!tpm_handler.increase_iteration(tpm_id)){
+                tpm_handler.reset_tpm(tpm_index);
+                tpm_reset = true;
             }
         }
+       
         if(tpm_found){
             std::stringstream ss;
-            ss << "1\t" << tpms.at(tpm_index).id() << "\t" << tpms.at(tpm_index).iteration() << "\n";
+            if(tpm_reset){                                                                     // 1 = stop 
+                ss << "4\t" << tpm_id << "\t" << tpm_handler.get_iteration(tpm_id) << "\t" << 0 << "\n";    
+            }else{
+                ss << "1\t" << tpm_id << "\t" << tpm_handler.get_iteration(tpm_id) << "\n";
+            }
             std::cout << "on_sync: " << ss.str() << std::endl;
             do_write(ss.str());
         } else{ // must be new machine? but shouldnt be
@@ -272,22 +385,28 @@ private:
     
     void on_linking(std::vector<std::string> & parsed_msg){
         bool tpm_found = false;
-        int tpm_index = -1;
-        
-             
-        
-        for(int i=0; i < tpms.size(); i++){
-            if(tpms.at(i).id() == std::stoi(parsed_msg.at(3))){
-                tpms.at(i).set_partner(std::stoi(parsed_msg.at(1)));
-                tpms.at(i).iteration_increase();
-                tpm_found = true;
-                tpm_index = i;
-                break;
+        bool tpm_reset = false;
+        int tpm_index = tpm_handler.find_tpm(std::stoi(parsed_msg.at(1)), false);
+        int tpm_id = -1;
+        if(tpm_index != -1){
+            tpm_found = true;
+            tpm_id = tpm_handler.getid(tpm_index);
+            if(!tpm_handler.increase_iteration(tpm_id)){
+              tpm_handler.reset_tpm(tpm_index);
+              tpm_found = true;
+
             }
-        }
+            tpm_handler.set_partner(tpm_id, std::stoi(parsed_msg.at(1)));
+        }     
+        
+       
         if(tpm_found){
             std::stringstream ss;
-            ss << "1\t" << tpms.at(tpm_index).id() << "\t" << tpms.at(tpm_index).iteration() << "\n";
+            if(tpm_reset){                                                                     // 1 = stop 
+                ss << "4\t" << tpm_id << "\t" << tpm_handler.get_iteration(tpm_id) << "\t" << 0 << "\n";    
+            }else{
+            ss << "1\t" << tpm_id << "\t" << tpm_handler.get_iteration(tpm_id) << "\n";
+            }
             std::cout << "on_linking: " << ss.str() << std::endl;
             do_write(ss.str());
         }
@@ -299,6 +418,37 @@ private:
         
         
     }
+    
+    void on_reset(std::vector<std::string> & parsed_msg){
+        bool tpm_found = false;      
+        int tpm_index = tpm_handler.find_tpm(std::stoi(parsed_msg.at(1)), false);
+        int tpm_id = -1;
+        if(tpm_index != -1){
+            tpm_found = true;
+            tpm_id = tpm_handler.getid(tpm_index);
+            tpm_handler.reset_tpm(tpm_index);
+            
+            
+            tpm_handler.increase_iteration(tpm_id);
+            tpm_handler.set_partner(tpm_id, std::stoi(parsed_msg.at(1)));
+        }     
+        
+       
+        if(tpm_found){
+            std::stringstream ss;
+            ss << "1\t" << tpm_id << "\t" << tpm_handler.get_iteration(tpm_id) << "\n";
+            std::cout << "on_reset: " << ss.str() << std::endl;
+            do_write(ss.str());
+        }
+        else{ // something is not right
+            
+            std::cout << "on_reset no tpm found" << std::endl;
+            
+        }
+       
+    }
+    
+ 
     
     
     
@@ -341,7 +491,7 @@ private:
     boost::posix_time::ptime last_ping_;
     bool peers_changed_;
     bool sock_using_ep;
-    std::vector<single_tpm_network_handler> tpms;
+    tpm_network_handler tpm_handler;
 };
 
 void update_peers_changed() {
